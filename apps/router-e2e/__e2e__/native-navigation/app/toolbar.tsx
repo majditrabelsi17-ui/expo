72e91435484c4a2be762c7fc3f58d9f9b116ab96
Jakub Tkacz <jtkacz@expo.io>
2026-03-16 17:01:27 +0100

    [expo-router][android] add support for menus in toolbar

--- apps/router-e2e/__e2e__/native-navigation/app/toolbar.tsx
+++ apps/router-e2e/__e2e__/native-navigation/app/toolbar.tsx
@@ -355,6 +355,17 @@ export default function ToolbarScreen() {
           </Pressable>
         </Stack.Toolbar.View>
 
+        <ActionsMenu
+          hidden={!showMenu}
+          image={image2}
+          emailsArchived={emailsArchived}
+          onArchiveToggle={handleArchiveToggle}
+          notificationsEnabled={notificationsEnabled}
+          onNotificationsToggle={handleNotificationsToggle}
+          favoriteColors={favoriteColors}
+          onColorSelect={handleColorSelect}
+        />
+
         {/* Xcasset button */}
         <Stack.Toolbar.Button
           hidden={!showXcassetButton}
@@ -377,17 +388,6 @@ export default function ToolbarScreen() {
         )}
 
         {/* Nested menu with dynamic content */}
-        {showMenu && (
-          <ActionsMenu
-            image={image2}
-            emailsArchived={emailsArchived}
-            onArchiveToggle={handleArchiveToggle}
-            notificationsEnabled={notificationsEnabled}
-            onNotificationsToggle={handleNotificationsToggle}
-            favoriteColors={favoriteColors}
-            onColorSelect={handleColorSelect}
-          />
-        )}
 
         {/* Flexible spacer at the end */}
         <Stack.Toolbar.Spacer />
@@ -404,6 +404,7 @@ function ActionsMenu({
   onNotificationsToggle,
   favoriteColors,
   onColorSelect,
+  hidden,
 }: {
   image: ReturnType<typeof useImage>;
   emailsArchived: boolean;
@@ -412,17 +413,34 @@ function ActionsMenu({
   onNotificationsToggle: () => void;
   favoriteColors: ('red' | 'blue' | 'green')[];
   onColorSelect: (color: 'red' | 'blue' | 'green') => void;
+  hidden?: boolean;
 }) {
   return (
-    <Stack.Toolbar.Menu icon="ellipsis.circle" title="Actions" tintColor={Color.ios.systemBrown}>
+    <Stack.Toolbar.Menu
+      hidden={hidden}
+      icon={
+        process.env.EXPO_OS === 'ios'
+          ? 'ellipsis.circle'
+          : require('../../../assets/android-icons/edit.xml')
+      }
+      title="Actions"
+      tintColor={Color.ios.systemBrown}>
       {/* Simple actions */}
       <Stack.Toolbar.MenuAction
-        icon="paperplane"
+        icon={
+          process.env.EXPO_OS === 'ios'
+            ? 'paperplane'
+            : require('../../../assets/android-icons/person.xml')
+        }
         onPress={() => Alert.alert('Send Email', 'Email sent succesiconully!')}>
         Send email
       </Stack.Toolbar.MenuAction>
       <Stack.Toolbar.MenuAction
-        icon="trash"
+        icon={
+          process.env.EXPO_OS === 'ios'
+            ? 'trash'
+            : require('../../../assets/android-icons/close.xml')
+        }
         destructive
         onPress={() => Alert.alert('Delete Email', 'Email deleted!')}>
         Delete email
